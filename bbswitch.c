7e792ef1768bbeeef59a3bcf58c1f6d08b987a9b
Lekensteyn <lekensteyn@gmail.com>
2011-12-30 15:49:01 +0100

    Bump module version to 0.2

--- bbswitch.c
+++ bbswitch.c
@@ -19,7 +19,7 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Toggle the discrete graphics card");
 MODULE_AUTHOR("Peter Lekensteyn <lekensteyn@gmail.com>");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 
 extern struct proc_dir_entry *acpi_root_dir;
 
