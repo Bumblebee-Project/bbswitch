ec42e5b7ad3643a94fedf325cd7ec21a0ad5e712
Lekensteyn <lekensteyn@gmail.com>
2012-03-01 23:41:56 +0100

    Add LENOVO IDEAPAD Y570 "PIQY0"

--- acpi-handle-hack.c
+++ acpi-handle-hack.c
@@ -21,7 +21,8 @@ static acpi_handle orig_handle;
  */
 static bool __init need_acpi_handle_hack(void) {
 	return dmi_match(DMI_PRODUCT_VERSION, "Lenovo IdeaPad Y470             ") ||
-		dmi_match(DMI_PRODUCT_VERSION, "Lenovo IdeaPad Y570             ");
+		dmi_match(DMI_PRODUCT_VERSION, "Lenovo IdeaPad Y570             ") ||
+		dmi_match(DMI_PRODUCT_VERSION, "LENOVO IDEAPAD Y570 ") /* sys-product-name: PIQY0 */;
 }
 
 static struct pci_dev __init *get_discrete_device(void) {
