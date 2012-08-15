f4128e5b40c008c1b2171debea4f82317e217727
Peter Wu <lekensteyn@gmail.com>
2012-08-15 12:54:31 +0200

    acpi-handle-hack: Add Lenovo G580
    Reported at Bumblebee-Project/Bumblebee#230, it seems to be the same
    issue.

--- acpi-handle-hack.c
+++ acpi-handle-hack.c
@@ -26,6 +26,7 @@ static bool __init need_acpi_handle_hack(void) {
 		|| dmi_match(DMI_PRODUCT_VERSION, "LENOVO IDEAPAD Y570 ") /* sys-product-name: PIQY0 */
 		|| dmi_match(DMI_PRODUCT_VERSION, "Lenovo IdeaPad Y580")
 		|| dmi_match(DMI_PRODUCT_VERSION, "PSPLBE-01V00HFR") /* TOSHIBA SATELLITE P870 */
+		|| dmi_match(DMI_PRODUCT_VERSION, "Lenovo G580")
 		;
 }
 
