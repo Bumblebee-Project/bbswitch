0c38f97c3a647aebbc298b830ac7496bfcb86a7d
Peter Wu <lekensteyn@gmail.com>
2013-12-05 00:22:06 +0100

    Update to version 0.8, update NEWS

--- bbswitch.c
+++ bbswitch.c
@@ -36,7 +36,7 @@
 #include <linux/seq_file.h>
 #include <linux/pm_runtime.h>
 
-#define BBSWITCH_VERSION "0.7"
+#define BBSWITCH_VERSION "0.8"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Toggle the discrete graphics card");
